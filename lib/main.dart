5fda5bfb39ac201b9f1b506d7e5aa570a214c2b2
Geethu krishna <geethukrishnasreekutty@gmail.com>
2026-04-10 04:48:14 +0530

    feat:change in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proteinova_connect/features/auth/presentation/signup_screen.dart';
+import 'package:proteinova_connect/features/sales/presentation/sales.dart';
 
 void main() {
   runApp(const MyApp());
