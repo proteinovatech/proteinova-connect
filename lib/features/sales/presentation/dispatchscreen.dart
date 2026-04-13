19e5870cd6f99150e41c24cfee8f787973d542fc
Arthi_001 <arthi004j@gmail.com>
2026-04-13 09:30:55 +0530

    dispatch screen created

---
+++ lib/features/sales/presentation/dispatchscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Dispatchscreen extends StatefulWidget {
+  const Dispatchscreen({super.key});
+
+  @override
+  State<Dispatchscreen> createState() => _DispatchscreenState();
+}
+
+class _DispatchscreenState extends State<Dispatchscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
