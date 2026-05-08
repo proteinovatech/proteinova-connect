fa56da9851648f9a05666ac84320474c94c4ed3e
abinraj207-max <abinraj207@gmail.com>
2026-05-08 16:36:50 +0530

    feat:testing

--- lib/features/addexpense/presentation/expense_management/widget/expensemodel_data.dart
+++ lib/features/addexpense/presentation/expense_management/widget/expensemodel_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:proteinova_connect/features/addexpense/presentation/expense_management/widget/expensemodel.dart';
+
+import 'package:proteinova_connect/features/branch/addexpense/presentation/expense_management/widget/expensemodel.dart';
 
 final List<ExpenseModel> expenses = [
   ExpenseModel(
@@ -42,7 +43,7 @@ final List<ExpenseModel> expenses = [
     status: "Paid",
     icon: Icons.home,
   ),
- 
+
   ExpenseModel(
     date: "09 Apr 2025",
     category: "Maintenance",
