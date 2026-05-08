9e9214a3460b66e0835493d4f059425f706cb9d1
abinraj207-max <abinraj207@gmail.com>
2026-05-08 10:40:34 +0530

    feat:testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:proteinova_connect/auth_bloc_provider.dart';
+import 'package:proteinova_connect/branch_bottom_navigator.dart';
 import 'package:proteinova_connect/features/auth/presentation/signup_screen.dart';
-import 'package:proteinova_connect/features/auth/presentation/splashscreen.dart';
+import 'package:proteinova_connect/purchase_bottom_navigator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
 
-  runApp(AppBlocProvider(child: const MyApp()));
+  final prefs = await SharedPreferences.getInstance();
+
+  final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  final String role = prefs.getString('role') ?? '';
+
+  Widget startScreen;
+
+  if (isLoggedIn) {
+    if (role == 'purchase') {
+      startScreen = const PurchaseBottomNavigator();
+    } else {
+      startScreen = const BranchBottomNavigator();
+    }
+  } else {
+    startScreen = const SignupScreen();
+  }
+
+  runApp(AppBlocProvider(child: MyApp(startScreen: startScreen)));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Widget? startScreen;
+
+  const MyApp({super.key, this.startScreen});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       routes: {"/signup": (context) => const SignupScreen()},
-      home: const SignupScreen(),
+
+      home: startScreen ?? const SignupScreen(),
     );
   }
 }
