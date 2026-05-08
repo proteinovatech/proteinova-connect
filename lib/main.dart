27726faa81c577168fd058b203de99c7384c8ff6
Geethu krishna <geethukrishnasreekutty@gmail.com>
2026-05-08 11:01:18 +0530

    admin inventory page created

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:proteinova_connect/admin_inventory/presentation/admin_inventory.dart';
 
 import 'package:proteinova_connect/core/services/app_bloc.dart';
 import 'package:proteinova_connect/features/branch/branch_bottom_navigator.dart';
