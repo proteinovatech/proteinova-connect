88f3c28dec2c3ff0f327d56f3cc4dc1f8cc60ee2
abinraj207-max <abinraj207@gmail.com>
2026-05-09 12:53:59 +0530

    feat:testing

--- lib/main.dart
+++ lib/main.dart
@@ -69,48 +69,20 @@ Future<void> main() async {
   runApp(AppBlocProvider(child: MyApp(startScreen: startScreen)));
 }
 
-<<<<<<< HEAD
-// class MyApp extends StatelessWidget {
-//   final Widget startScreen;
-
-//   const MyApp({super.key, required this.startScreen});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-
-//       routes: {
-//         "/signup": (context) => const SignupScreen(),
-//       },
-
-//       home: startScreen,
-//     );
-//   }
-// }
-=======
->>>>>>> b3ea674c84feb8ed5b59a7f750a00b966dc36fd7
 class MyApp extends StatelessWidget {
   final Widget? startScreen;
 
-
   const MyApp({super.key, this.startScreen});
- 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-<<<<<<< HEAD
-      // routes: {"/signup": (context) => const SignupScreen()},
-=======
       routes: {"/signup": (context) => const SignupScreen()},
->>>>>>> b3ea674c84feb8ed5b59a7f750a00b966dc36fd7
 
       // home: startScreen ?? const SignupScreen(),
       home: AdminBottomNavigator(),
     );
   }
 }
-
