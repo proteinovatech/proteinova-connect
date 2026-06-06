c96e019d923bd19f970f70147ac58a28258d12c0
Arthi_001 <arthi004j@gmail.com>
2026-06-06 16:26:10 +0530

    feat: warehouse stock page created

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:proteinova_connect/admin_bottom_navigator.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:proteinova_connect/features/purchase/warehouse_stock_updates.dart';
 import 'package:proteinova_connect/firebase_options.dart';
 
 import 'package:proteinova_connect/core/services/app_bloc.dart';
